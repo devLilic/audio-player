00bd3e47c703f4ff9088a39d20013fec4afc4165
lilic7 <zubcolilian@mail.ru>
2022-05-19 18:34:31 +0300

    Basic controls are working

--- script.js
+++ script.js
@@ -1,51 +1,46 @@
-const audioElement = document.getElementById('audio')
-
 class Player {
     track = null;
     gainNode = null;
-    isPlaying = false;
     currentSoundLevel = 1;
-    audioDuration = null;
 
     // controls
     controls = {
         audioElement: {
             id: 'audio',
             events: {
-                onloadedmetadata: this.changeProgressMaxDuration
+                ontimeupdate: this.showCurrentTime,
+                onloadedmetadata: this.showTotalDuration,
+                onended: this.pause
             }
         },
         playBtn: {
             id: 'playBtn',
             events: {
-                onclick: this.play
+                onclick: this.play,
             }
         },
-        muteBtn: {
-            id: 'muteSound',
-            events: {
-                onclick: this.mute
-            }
+        // muteBtn: {
+        //     id: 'muteSound',
+        //     events: {
+        //         onclick: this.mute
+        //     }
+        // },
+        // volume: {
+        //     id: 'volume',
+        //     events: {
+        //         oninput: this.changeVolume
+        //     }
+        // },
+        progressBar: {
+            id: 'progress'
         },
-        volume: {
-            id: 'volume',
-            events: {
-                oninput: this.changeVolume
-            }
+        currentDuration: {
+            id: 'current-duration'
         },
-        progress: {
-            id: 'progress-input',
-            events: {
-                oninput: this.audioProgress
-            }
-        },
-        playTime: {
-            id: 'play-time-indicator'
-        },
-        totalTime: {
-            id: 'progress-total-time-indicator'
+        totalDuration: {
+            id: 'total-duration'
         }
-    }
+    };
 
     constructor() {
         this.initPlayer();
@@ -74,6 +69,9 @@ class Player {
             }
             this.controls[item] = element;
         }
+        this.controls.progressBar.addEventListener('click', ev => {
+            this.goToPosition(ev)
+        })
     }
 
     setCurrentSong(songSrc) {
@@ -84,51 +82,68 @@ class Player {
 
     play() {
         this.audioContext.resume();
-        if (!this.isPlaying) {
-            this.controls.audioElement.play()
-            this.controls.playBtn.children[0].classList.replace("fa-play", "fa-pause")
-        } else {
-            this.controls.audioElement.pause();
-            this.controls.playBtn.children[0].classList.replace("fa-pause", "fa-play")
-        }
-        this.isPlaying = !this.isPlaying;
+        this.controls.audioElement.play()
+        this.controls.playBtn.children[0].classList.replace("fa-play", "fa-pause")
+        this.controls.playBtn.onclick = () => {
+            this.pause()
+        };
     }
 
-    mute() {
-        if (this.gainNode.gain.value !== 0) {
-            this.currentSoundLevel = this.gainNode.gain.value;
-            this.gainNode.gain.value = 0;
-            this.controls.muteBtn.children[0].classList.value = "fa-solid fa-volume-high"
-            this.controls.volume.value = 0;
-        } else if (this.gainNode.gain.value === 0 && this.currentSoundLevel !== 0) {
-            this.gainNode.gain.value = this.currentSoundLevel;
-            this.controls.volume.value = this.currentSoundLevel;
-            this.currentSoundLevel = 0;
-            this.controls.muteBtn.children[0].classList.value = "fa-solid fa-volume-xmark"
-        }
+    pause() {
+        this.controls.audioElement.pause();
+        this.controls.playBtn.children[0].classList.replace("fa-pause", "fa-play")
+        this.controls.playBtn.onclick = () => {
+            this.play()
+        };
     }
 
-    changeVolume() {
-        this.gainNode.gain.value = this.controls.volume.value;
+    showCurrentTime() {
+        let duration = this.controls.audioElement.currentTime
+        this.controls.currentDuration.innerText = this.toTime(duration);
+        this.updateProgressBar()
     }
 
-    audioProgress(){
-        let playTime = this.controls.progress.value;
-        this.controls.audioElement.currentTime = playTime;
-        this.controls.playTime.innerText = Math.round(playTime);
+    updateProgressBar() {
+        let progress = this.controls.audioElement.currentTime / this.controls.audioElement.duration * 100;
+        this.controls.progressBar.children[0].style.width = `${progress}%`;
     }
 
-    changeProgressMaxDuration() {
+    goToPosition(ev) {
+        let newPosition = ev.offsetX / this.controls.progressBar.clientWidth
+        this.controls.progressBar.children[0].style.width = `${newPosition * 100}%`;
+        this.controls.audioElement.currentTime = this.controls.audioElement.duration * newPosition;
+    }
+
+    // mute() {
+    //     if (this.gainNode.gain.value !== 0) {
+    //         this.currentSoundLevel = this.gainNode.gain.value;
+    //         this.gainNode.gain.value = 0;
+    //         this.controls.muteBtn.children[0].classList.value = "fa-solid fa-volume-high"
+    //         this.controls.volume.value = 0;
+    //     } else if (this.gainNode.gain.value === 0 && this.currentSoundLevel !== 0) {
+    //         this.gainNode.gain.value = this.currentSoundLevel;
+    //         this.controls.volume.value = this.currentSoundLevel;
+    //         this.currentSoundLevel = 0;
+    //         this.controls.muteBtn.children[0].classList.value = "fa-solid fa-volume-xmark"
+    //     }
+    // }
+    //
+    // changeVolume() {
+    //     this.gainNode.gain.value = this.controls.volume.value;
+    // }
+    showTotalDuration() {
         const duration = this.controls.audioElement.duration;
-        this.controls.progress.setAttribute("max", duration)
-        this.controls.totalTime.innerText = Math.round(duration);
+        this.controls.totalDuration.innerText = this.toTime(duration);
+    }
+
+    toTime(seconds) {
+        let minutes = Math.floor(seconds / 60);
+        minutes = (minutes < 10 ? '0' : '') + minutes;
+        seconds %= 60;
+        seconds = (seconds < 10 ? '0' : '') + Math.floor(seconds)
+        return `${minutes}:${seconds}`;
     }
 }
 
 const player = new Player()
-player.setCurrentSong('music/jacinto-1.mp3');
-
-
-// what to do when audio file ended to play
-audioElement.addEventListener('ended', function (event) {
-});
+player.setCurrentSong('music/jacinto-3.mp3');
